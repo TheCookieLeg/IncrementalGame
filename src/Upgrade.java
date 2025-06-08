219f0ba1f3dc53899b37239f84c5aed4f5a8dffb
TheCookieLegend <lucas2.olafsen@gmail.com>
2025-06-08 23:51:45 +0200

    Constructor in upgrade class

--- src/Upgrade.java
+++ src/Upgrade.java
@@ -1,10 +1,20 @@
+import javax.swing.*;
+
 public class Upgrade {
     private String nameOfUpgrade;
-    private double totalAmount;
     private double amountPerUpgrade;
+    private double costPerUpgrade;
 
-    public Upgrade() {
+    private double totalAmount;
 
+    private JButton upgradeButton;
+
+    public Upgrade(String nameOfUpgrade, double amountPerUpgrade, double costPerUpgrade) {
+        this.nameOfUpgrade = nameOfUpgrade;
+        this.amountPerUpgrade = amountPerUpgrade;
+        this.costPerUpgrade = costPerUpgrade;
     }
 
+    private JButton getUpgradeButton() {return upgradeButton;}
+    
 }
